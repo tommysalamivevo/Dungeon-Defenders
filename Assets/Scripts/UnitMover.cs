41813046e61623a28e0f4b962aecac464ccaafbb
tommysalamivevo <78112780+tommysalamivevo@users.noreply.github.com>
2023-03-07 18:35:50 -0600

    implemented things
    the guy does a funny jump

---
+++ Assets/Scripts/UnitMover.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitMover : MonoBehaviour
+{
+    // Public variables
+    public Transform[] waypoints;
+    public float speed = 5f;
+    public AudioSource audioSource;
+    public AudioClip soundEffect;
+
+    // Private variables
+    private int currentWaypoint = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Invoke the PlaySoundEffect method every 6 seconds, starting after 1 second
+        InvokeRepeating("PlaySoundEffect", 1f, 6f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Calculate distance to next waypoint
+        float distance = Vector3.Distance(transform.position, waypoints[currentWaypoint].position);
+
+        // If we are close enough to the current waypoint, move to the next one
+        if (distance < 0.1f)
+        {
+            currentWaypoint++;
+
+            // If we have reached the end of the track, reset to the beginning
+            if (currentWaypoint >= waypoints.Length)
+            {
+                currentWaypoint = 0;
+            }
+        }
+
+        // Move towards the current waypoint
+        transform.position = Vector3.MoveTowards(transform.position, waypoints[currentWaypoint].position, speed * Time.deltaTime);
+    }
+
+public AudioClip[] soundEffects; // an array of audio clips to play
+private int currentSoundEffectIndex = 0; // the index of the current sound effect to play
+
+void PlaySoundEffect()
+{
+    audioSource.PlayOneShot(soundEffects[currentSoundEffectIndex]);
+    currentSoundEffectIndex = (currentSoundEffectIndex + 1) % soundEffects.Length; // toggle to the next sound effect in the array
+}
+}
